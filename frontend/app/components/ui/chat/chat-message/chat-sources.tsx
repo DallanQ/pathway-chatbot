64edea84842c41747b1a2ba08c2a5fa95aa62a8f
Bamidele Olawale Ajayi <81062134+baajayi@users.noreply.github.com>
2024-08-22 17:19:27 +0100

    show document links

--- frontend/app/components/ui/chat/chat-message/chat-sources.tsx
+++ frontend/app/components/ui/chat/chat-message/chat-sources.tsx
@@ -42,7 +42,11 @@ export function ChatSources({ data }: { data: SourceData }) {
       <div className="font-semibold text-lg">Sources:</div>
       <ul style={{listStyleType: "disc", marginLeft: "20px"}}>
         {documents.map((document) => {
-          return <li><a href={document.url} key={document.url}>{document.url}</a></li>;
+          return (
+            <li key={document.url}>
+              <a href={document.url}>{document.url}</a>
+            </li>
+          );
           // return <DocumentInfo key={document.url} document={document} />;
         })};
       </ul>
