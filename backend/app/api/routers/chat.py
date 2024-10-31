4d16ab64781debdfa0b9e9ac3c28580edea4e7c1
Bamidele Olawale Ajayi <81062134+baajayi@users.noreply.github.com>
2024-10-31 05:16:58 -0600

    added logging

--- backend/app/api/routers/chat.py
+++ backend/app/api/routers/chat.py
@@ -69,6 +69,7 @@ async def chat(
         )
         
         trace_id = langfuse_context.get_current_trace_id()
+        logger.info(f"We got the trace id to be : {trace_id)}")
         
         return VercelStreamResponse(request, event_handler, response, data, tokens, trace_id=trace_id)
         # return VercelStreamResponse(request, event_handler, response, data, tokens)
@@ -110,6 +111,7 @@ async def chat_request(
     
     # Get the trace_id of Langfuse
     trace_id = langfuse_context.get_current_trace_id()
+    logger.info(f"We got the trace id to be : {trace_id)}")
 
     # Delete the chat_history from the chat_engine
     chat_engine.reset()
