545e2b7f49e67f01e356907d3a38b3831ebb3652
Alirio Mieres <mie21001@byui.edu>
2024-12-19 10:53:22 -0400

    change the position of hints link

--- frontend/app/components/header.tsx
+++ frontend/app/components/header.tsx
@@ -1,4 +1,9 @@
+import { ExternalLink } from "lucide-react";
+
 export default function Header() {
+
+  const hints = "https://missionaries.prod.byu-pathway.psdops.com/How-to-use-the-Missionary-Assistant"
+
   return (
     <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
       {/* <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
@@ -7,7 +12,7 @@ export default function Header() {
       </p> */}
       <div className="bottom-0 left-0 mb-4 h-auto w-full from-white via-white dark:from-black dark:via-black lg:static lg:w-auto bg-none lg:mb-0">
         <p
-          className="font-nunito text-xl font-bold gap-2 pY-7 md:p-0"
+          className="font-nunito text-xl font-bold pY-7 md:p-0 flex items-center gap-4"
         >
           <span>Missionary Assistant</span>
           {/* <Image
@@ -18,6 +23,7 @@ export default function Header() {
             height={40}
             priority
           /> */}
+          <a href={hints} target="_blank" className="font-nunito text-sm flex gap-1 text-blue-600 visited:text-purple-600">Hints <span><ExternalLink size={16}/></span></a>
         </p>
       </div>
     </div>
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,7 @@
 import Header from "@/app/components/header";
 import ChatSection from "./components/chat-section";
-import { ExternalLink } from "lucide-react";
 
 export default function Home() {
-
-  const hints = "https://missionaries.prod.byu-pathway.psdops.com/How-to-use-the-Missionary-Assistant"
-
   return (
     <main className="h-dvh w-screen flex justify-center items-center background-gradient">
       <div className="space-y-2 lg:space-y-4 w-[90%] lg:w-[60rem]">
@@ -14,7 +10,7 @@ export default function Home() {
           <ChatSection />
         </div>
         <p className="mt-5 text-sm flex gap-4 justify-between">
-          <span>Powered by BYU-Pathway Software Development Students</span><a href={hints} target="_blank" className="flex gap-1 text-blue-600 visited:text-purple-600">Hints <span><ExternalLink size={16}/></span></a>
+          <span>Powered by BYU-Pathway Software Development Students</span>
         </p>
       </div>
     </main>
