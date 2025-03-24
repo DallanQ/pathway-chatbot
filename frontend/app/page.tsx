fe1312c833eea8bc6c59243fb79fdac0d6a9442d
Alirio Mieres <mie21001@byui.edu>
2025-03-23 22:49:28 -0400

    [UPDATE] adjust chat section height and enhance footer with logo

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,17 +1,24 @@
 import Header from "@/app/components/header";
 import ChatSection from "./components/chat-section";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="h-dvh w-screen flex justify-center items-center background-gradient">
       <div className="space-y-2 lg:space-y-4 w-[90%] lg:w-[60rem]">
         <Header />
-        <div className="h-[80dvh] flex pb-8">
+        <div className="h-[75dvh] flex pb-8">
           <ChatSection />
         </div>
-        <p className="mt-5 text-sm flex gap-4 justify-between">
-          <span>Powered by BYU-Pathway Software Development Students</span>
-        </p>
+        <div className="mt-5 pb-3 text-sm flex gap-4 justify-between items-start">
+          <p>Powered by BYU-Pathway Software Development Students</p>
+          <Image 
+            src="/byu-pw-stackedgray.svg" 
+            alt="BYU Pathway Logo" 
+            width={100}
+            height={100} 
+          />
+        </div>
       </div>
     </main>
   );
