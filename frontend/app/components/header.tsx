9b22daf88df7f9edf657348bb5d87e9898fefb4c
Alirio Mieres <mie21001@byui.edu>
2025-06-27 16:01:38 -0400

    [UPDATE] enhance header layout with responsive design and improved element positioning

--- frontend/app/components/header.tsx
+++ frontend/app/components/header.tsx
@@ -4,20 +4,25 @@ export default function Header() {
   const hints = "https://missionaries.prod.byu-pathway.psdops.com/How-to-use-the-Missionary-Assistant"
 
   return (
-    <header className="w-full h-[60px] bg-[#FFC328] flex items-center justify-between px-6 shadow-none">
-      <div className="flex items-center h-full">
-        <img 
-          src="/pathway-horizontal-logo.png" 
-          alt="BYU Pathway Logo" 
-          width={140}
-          height={40}
-        />
-      </div>
-      <div className="flex-1 flex justify-center items-center h-full">
-        <span className="font-nunito text-lg font-semibold text-[#454540]">Missionary Assistant</span>
-      </div>
-      <div className="flex items-center h-full">      
-        <a href={hints} target="_blank" className="text-base flex items-center gap-1 text-black hover:text-blue-700 font-semibold">Hints <ExternalLink size={16}/></a>
+    <header className="w-full bg-[#FFC328] px-3 md:px-6 shadow-none md:py-3">
+      <div className="flex flex-col md:flex-row md:items-center md:justify-between h-full py-2 md:py-0">
+        <div className="w-full flex flex-row items-center justify-between md:justify-between md:w-full">
+          <div className="flex items-center">
+            <img 
+              src="/pathway-horizontal-logo.png" 
+              alt="BYU Pathway Logo" 
+              className="w-[100px] h-auto md:w-[140px]"
+            />
+          </div>
+          <span className="hidden md:block font-nunito text-lg font-semibold text-[#454540] text-center flex-1">Missionary Assistant</span>
+          <a href={hints} target="_blank" className="text-[#454540] font-semibold flex items-center gap-1 text-sm md:text-base hover:text-blue-700">
+            <span className="hidden md:inline">Hints</span>
+            <ExternalLink size={18}/>
+          </a>
+        </div>
+        <div className="w-full flex justify-center mt-1 md:hidden">
+          <span className="font-nunito text-base font-semibold text-[#454540] text-center">Missionary Assistant</span>
+        </div>
       </div>
     </header>
   );
