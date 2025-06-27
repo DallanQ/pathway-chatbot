183a468a5190e47ff7bc37a5ea64331a271d737b
Alirio Mieres <mie21001@byui.edu>
2025-06-27 14:56:41 -0400

    [UPDATE] refine layout of main page and adjust chat messages container styling

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="h-dvh w-screen flex justify-center items-center background-gradient">
-      <div className="space-y-2 lg:space-y-4 w-[90%] lg:w-[60rem]">
+      <div className="w-[90%] lg:w-[60rem]">
         <Header />
         <div className="h-[75dvh] flex pb-8">
           <ChatSection />
