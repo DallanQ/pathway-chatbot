cdb085d37d1c41e80d89d5fdb3a49dd5dd00480b
IsaiasZc <coroxerox2@gmail.com>
2025-07-08 09:35:42 -0500

    improve  important footer message

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
           <ChatSection />
         </div>
         <div className="mt-5 pb-3 text-sm flex gap-4 justify-between items-start">
-          <p className="text-red-500">IMPORTANT: This website is intended for missionaries assigned to BYU-Pathway and is not for student use. We ask that you do not share or promote on social media. Encourage students to use the Companion app found in their student portal. Thank you for respecting this guideline.
+          <p className="text-red-500">IMPORTANT: This website is intended for missionaries assigned to BYU-Pathway and is not for student use. Encourage students to use the Companion app found in their student portal. We ask that you do not share or promote on social media.  Thank you for respecting this guideline.
           </p>
         </div>
       </div>
