159fd83fb24350948da19eff3b22b1d8d7e35afc
Aghadiaye Ernest <ernestjacob789@gmail.com>
2025-09-22 05:52:44 -0700

    deploy issue

--- frontend/app/api/ip/route.ts
+++ frontend/app/api/ip/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request: NextRequest) {
   const ip = request.headers.get('x-forwarded-for') || request.ip;
 
